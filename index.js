177ac4a9e6171ac68968f0ee19b49f239941e504
taka.hiroto <takaih.dev57@gmail.com>
2022-03-26 16:16:23 +0900

    restAPI実装

---
+++ index.js
@@ -0,0 +1,12 @@
+// expressモジュールを読み込む
+const express = require("express");
+const app = express();
+
+const userList = [
+		{id: 1, name: "田中", age: 24, personalColor: "blue"},
+		{id: 2, name: "先岡", age: 28, personalColor: "pink"},
+		{id: 3, name: "斎藤", age: 24, personalColor: "green"},
+	];
+app.get("/", (req, res) => res.json(userList));
+
+app.listen(9000, () => console.log("Listen on port 9000"));
